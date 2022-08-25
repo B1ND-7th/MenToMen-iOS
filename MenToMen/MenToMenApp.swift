2c97b97ab9dabf93519338420d62d78fae7422e4
이석호 <mercen@mercen.net>
2022-08-25 20:42:49 +0900

    Initial Commit

---
+++ MenToMen/MenToMenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenToMenApp.swift
+//  MenToMen
+//
+//  Created by Mercen on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MenToMenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
