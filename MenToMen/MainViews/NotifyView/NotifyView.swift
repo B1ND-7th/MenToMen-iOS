8cb302666546083df0da0c67e7f28cbc436372ba
이석호 <mercen@mercen.net>
2022-10-12 20:36:29 +0900

    Updated Menubar for NotifyView

--- MenToMen/MainViews/NotifyView/NotifyView.swift
+++ MenToMen/MainViews/NotifyView/NotifyView.swift
@@ -14,29 +14,54 @@ struct NotifyView: View {
     let profileUrl: String? = "http://dodam.b1nd.com/api/image/png/DW_IMG_89280208781.png"
     let name: String = "이석호"
     var body: some View {
-        ScrollView {
-            HStack {
-                CachedAsyncImage(url: URL(string: profileUrl ?? "")) { image in
-                    image
-                        .resizable()
-                } placeholder: {
-                    if profileUrl == nil {
-                        Image("profile")
+        ZStack {
+            ScrollView {
+                HStack {
+                    CachedAsyncImage(url: URL(string: profileUrl ?? "")) { image in
+                        image
                             .resizable()
-                    } else {
-                        NothingView()
+                    } placeholder: {
+                        if profileUrl == nil {
+                            Image("profile")
+                                .resizable()
+                        } else {
+                            NothingView()
+                        }
+                    }
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                    VStack {
+                        Text("**\(name)**님이 회원님의 게시글에 답글을 남겼습니다.")
+                        Text("\"엄준식은 살아있다!!!!\"")
                     }
                 }
-                .frame(width: 40, height: 40)
-                .clipShape(Circle())
-                VStack {
-                    Text("**김종윤**님이 회원님의 게시글에 답글을 남겼습니다.")
-                    Text("\"엄준식은 살아있다!!!!\"")
-                }
+                .customCell()
+                .padding(.top, 61)
             }
-            .customCell()
+            .customList()
+            HStack {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image("back")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color(.label))
+                }
+                .frame(width: 30, height: 30)
+                .padding(.leading, 10)
+                Spacer()
+                Text("알림")
+                    .padding(.trailing, 40)
+                Spacer()
+            }
+            .padding(.top, topPadding)
+            .frame(height: topPadding + 61)
+            .background(Color(.secondarySystemGroupedBackground))
+            .setAlignment(for: .top)
+            .customShadow()
+            .edgesIgnoringSafeArea(.top)
         }
-        .customList()
         .dragGesture(dismiss, $dragOffset)
         .navigationBarHidden(true)
     }
